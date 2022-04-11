44868dd6f17eef314c809dd0e1bf669adba21c15
farkas45 <79808071+farkas45@users.noreply.github.com>
2022-04-11 19:51:31 +0200

    app class elkészítése
    app class elkészítése + konstruktorba példányositani view,Dao,Presentert+
    main metodus létrehozása alkalmazás inditása

--- src/hu/ruander/app/App.java
+++ src/hu/ruander/app/App.java
@@ -1,5 +1,27 @@
 package hu.ruander.app;
 
+import javax.swing.SwingUtilities;
+
+import hu.ruander.dao.EmployeeDao;
+import hu.ruander.presenter.EmployeePresenter;
+import hu.ruander.view.EmployeeView;
+
 public class App {
 
+	
+	public App() {
+		final EmployeeView view=new EmployeeView();
+		final EmployeeDao employeeDao = new EmployeeDao();
+		new EmployeePresenter(view,employeeDao);
+	}
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new App();
+			}
+		});
+		
+	}
 }
--- src/hu/ruander/presenter/EmployeePresenter.java
+++ src/hu/ruander/presenter/EmployeePresenter.java
@@ -1,5 +1,12 @@
 package hu.ruander.presenter;
 
+import hu.ruander.dao.EmployeeDao;
+import hu.ruander.view.EmployeeView;
+
 public class EmployeePresenter {
+
+	public EmployeePresenter(EmployeeView view, EmployeeDao employeeDao) {
+		// TODO Auto-generated constructor stub
+	}
 
 }
